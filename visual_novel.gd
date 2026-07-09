1133f5b3eb1a1909da1cd87e3ade83a9beeb831b
Dev Ramudit <dramudit@gmail.com>
2026-07-09 12:46:38 -0400

    Start on rpy parsing

--- visual_novel.gd
+++ visual_novel.gd
@@ -5,8 +5,27 @@ class_name VisualNovel extends Node
 var dialogue_lines = ["Hi", "How's it going?", "OK bye"]
 var dialogue_index = 0
 
+func process_script():
+	var characters = {}
+	
+	var file = FileAccess.open("res://text/script.txt", FileAccess.READ)
+	var content = file.get_as_text()
+	content = content.split("\n")
+	for line in content:
+		line.strip_edges()
+		if not line or line[0] == "#":
+			# comment, ignore
+			continue
+		var spline = line.split(" ")
+		# Handle definition of characters
+		if spline[0] == "define":
+			var words = spline.split('"')
+			
+		print(line)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	process_script()
 	dialogue_label.text = dialogue_lines[dialogue_index]
 	if get_tree().current_scene == self:
 		set_process_unhandled_key_input(true)
